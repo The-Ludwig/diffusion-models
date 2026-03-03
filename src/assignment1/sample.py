0c5a572af167118e583dfdf80e90e77eb200d06d
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2026-03-03 19:47:26 +0100

    cleanup

--- src/assignment1/sample.py
+++ src/assignment1/sample.py
@@ -1,14 +1,24 @@
 import torch
 from tqdm import tqdm
+from torchvision import utils as tvu
+from pathlib import Path
+from assignment1.model import UNet
 
+SAVE_STEPS = [300, 250, 200, 150, 100, 50, 0]
 
 @torch.no_grad()
 def sample(
-    model, n_samples, image_channels, image_size, T, alpha, alpha_cumprod, beta, device
+    model, n_samples, image_channels, image_size, T, alpha, alpha_cumprod, beta, device, 
+    save_t=None, save_path=None
 ):
     model.eval()
     # Start from pure noise
     x = torch.randn((n_samples, image_channels, image_size, image_size), device=device)
+
+    if save_t is not None and save_path is not None:
+        if T in save_t:
+            tvu.save_image(x, save_path/f"{T:03d}.png")
+
 
     for i in tqdm(reversed(range(T)), total=T, desc="Sampling", leave=False):
         t = torch.full((n_samples,), i, device=device, dtype=torch.long)
@@ -35,7 +45,35 @@ def sample(
 
         x = mean + std * noise
 
-    model.train()
+        model.train()
+
+        if save_t is not None and save_path is not None:
+            if i in save_t:
+                tvu.save_image(x, save_path/f"{i:03d}.png")
+
     # Rescale from [-1, 1] back to [0, 1] for visualization
     x = (x.clamp(-1, 1) + 1) / 2
     return x
+
+
+if __name__ == "__main__":
+    # Hyperparams (assume stays as trained)
+    T = 300
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # DDPM Schedule (assume stays as trained)
+    beta = torch.linspace(1e-4, 0.02, T).to(device)
+    alpha = 1.0 - beta
+    alpha_cumprod = torch.cumprod(alpha, dim=0)
+
+    # Load latest model 
+    model = UNet().to(device) 
+    model_weights = torch.load("ddpm_mnist_final.pth")
+
+    model.load_state_dict(model_weights['model'], strict=True)
+
+    # generate images
+    path = Path("solution_1/generated/")
+    path.mkdir(exist_ok=True, parents=True)
+    sample(model, 5, 1, 28, T, alpha, alpha_cumprod, beta, device, save_t=SAVE_STEPS, save_path=path)
+
